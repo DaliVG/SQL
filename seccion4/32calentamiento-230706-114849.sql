37a9d305fd727b65d847efceceb9ff3f0e608592
DaliVidic <daliborka_vg@hotmail.com>
2024-02-19 21:10:41 +0000

    exercise 32

--- seccion4/32calentamiento-230706-114849.sql
+++ seccion4/32calentamiento-230706-114849.sql
@@ -1,6 +1,19 @@
 -- Nombre, apellido e IP, donde la última conexión se dió de 221.XXX.XXX.XXX
-
-
+select
+    first_name,
+    last_name,
+    last_connection
+from
+    users
+where
+    last_connection like '221.%';
 
 -- Nombre, apellido y seguidores(followers) de todos a los que lo siguen más de 4600 personas
-
+select
+    first_name,
+    last_name,
+    followers
+from
+    users
+where
+    followers >= 4600;
